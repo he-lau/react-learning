c385c6f18d32c839baa1a49fb1eb8e0c56b6f8c4
he-lau <he.laurent.1130@gmail.com>
2023-12-20 16:26:20 +0100

    States : useState

--- react-introduction/src/Greetings.jsx
+++ react-introduction/src/Greetings.jsx
@@ -1,12 +1,28 @@
 // IMPORTANT : 'props' readonly !!!!!
 export function Greetings(props) {
   console.log(props);
+
+  let title = "Greetings component";
+  props.doSomething();
+
   return (
-    <ul>
-      <li>Hello {props.firstname}</li>
-      <li>!</li>
-      <li>{props.image}</li>
-      <li>{props.children}</li>
-    </ul>
+    <>
+      <h1>{title}</h1>
+      <ul>
+        <li>Hello {props.firstName}</li>
+        <li>!</li>
+        <li>{props.image}</li>
+        <li>{props.isSunny ? <ItsSunny /> : <ItsRainy />}</li>
+      </ul>
+      <div>{props.children}</div>
+    </>
   );
+}
+
+function ItsSunny() {
+  return <p>Il fait beau :D</p>;
+}
+
+function ItsRainy() {
+  return <p>Il fait moche :c</p>;
 }
